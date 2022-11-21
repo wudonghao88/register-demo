f95e84e63742265926a89a1fe793890101cefc9f
donghao <346813955@qq.com>
2022-11-21 12:36:27 +0800

    registerServer内容提交

---
+++ register-server/src/main/java/com/donghao/register/demo/register/server/controller/RegisterController.java
@@ -0,0 +1,10 @@
+package com.donghao.register.demo.register.server.controller;
+
+/**
+ * 这个controller是接收 register-client发送过来的请求
+ * 在springcloud Eureka中组件是jersey。
+ * jersey:restful框架，可以接收http请求
+ */
+public class RegisterController {
+//public void
+}
---
+++ register-server/src/main/java/com/donghao/register/demo/register/server/pojo/RegisterRequest.java
@@ -0,0 +1,36 @@
+package com.donghao.register.demo.register.server.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * 注册请求
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+public class RegisterRequest {
+    /**
+     * 服务器名称
+     */
+    private String serviceName;
+    /**
+     * 服务所在的主机名
+     */
+    private String hostName;
+    /**
+     * 服务所在机器的ip地址
+     */
+    private String ip;
+    /**
+     * 服务所在机器的port
+     */
+    private int port;
+    /**
+     * 服务实例id
+     */
+    private String serviceInstanceId;
+}

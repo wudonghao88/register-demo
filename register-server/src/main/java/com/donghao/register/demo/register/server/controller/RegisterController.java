adf7842603ed5ea961688a91a5ae7fb8f573331f
donghao <346813955@qq.com>
2022-11-22 16:27:27 +0800

    服务注册测试用例

--- register-server/src/main/java/com/donghao/register/demo/register/server/controller/RegisterController.java
+++ register-server/src/main/java/com/donghao/register/demo/register/server/controller/RegisterController.java
@@ -1,7 +1,7 @@
 package com.donghao.register.demo.register.server.controller;
 
-import com.donghao.register.demo.register.server.pojo.RegisterRequest;
-import com.donghao.register.demo.register.server.pojo.RegisterResponse;
+import com.donghao.register.demo.register.common.constants.ResponseStatusConstants;
+import com.donghao.register.demo.register.server.pojo.*;
 
 /**
  * 这个controller是接收 register-client发送过来的请求
@@ -9,13 +9,24 @@ import com.donghao.register.demo.register.server.pojo.RegisterResponse;
  * jersey:restful框架，可以接收http请求
  */
 public class RegisterController {
+
+    private Registry registry = Registry.getInstance();
+
     public RegisterResponse register(RegisterRequest request) {
         RegisterResponse registerResponse = new RegisterResponse();
         try {
-            registerResponse.setStatus()
-        }catch (Exception e) {
-
+            ServiceInstance serviceInstance = new ServiceInstance();
+            serviceInstance.setServiceName(request.getServiceName());
+            serviceInstance.setIp(request.getIp());
+            serviceInstance.setHostName(request.getHostName());
+            serviceInstance.setPort(request.getPort());
+            serviceInstance.setServiceInstanceId(request.getServiceInstanceId());
+            serviceInstance.setLease(new Lease());
+            registry.register(serviceInstance);
+            registerResponse.setStatus(ResponseStatusConstants.SUCCESS);
+        } catch (Exception e) {
+            registerResponse.setStatus(ResponseStatusConstants.FAILURE);
         }
-        return null;
+        return registerResponse;
     }
 }

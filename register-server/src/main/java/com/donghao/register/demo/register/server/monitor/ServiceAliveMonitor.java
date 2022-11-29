4eceb49cda8cf96a2803d512b8701dc176dd28dc
donghao <346813955@qq.com>
2022-11-29 12:38:02 +0800

    shutdown方法做成

--- register-server/src/main/java/com/donghao/register/demo/register/server/monitor/ServiceAliveMonitor.java
+++ register-server/src/main/java/com/donghao/register/demo/register/server/monitor/ServiceAliveMonitor.java
@@ -1,9 +1,9 @@
 package com.donghao.register.demo.register.server.monitor;
 
+import java.util.Map;
+
 import com.donghao.register.demo.register.server.pojo.Registry;
 import com.donghao.register.demo.register.server.pojo.ServiceInstance;
-
-import java.util.Map;
 
 /**
  * 微服务存货状态监控后台线程
@@ -11,75 +11,84 @@ import java.util.Map;
  * @author donghao.wu
  */
 public class ServiceAliveMonitor {
-    /**
-     * 服务实例过期时间
+  /**
+   * 服务实例过期时间
+   */
+  private static final Long TTL = 90 * 1000L;
+
+  private Daemon daemon;
+
+  private volatile boolean isRunning;
+
+  public ServiceAliveMonitor() {
+    this.daemon = new Daemon();
+    /*
+     * 设置了这个标志位，就代表这个线程是一个deamon线程，后台线程
+     * 非deamon线程，我们一般叫做工作线程
+     * 如果工作线程（main线程）结束了，deamon线程不会阻止jvm进程退出
+     * deamon线程会跟jvm一起关闭
      */
-    private static final Long TTL = 90 * 1000L;
-    private Daemon daemon;
+    daemon.setDaemon(true);
+    isRunning = true;
+  }
 
-    public ServiceAliveMonitor() {
-        this.daemon = new Daemon();
-        /*
-        设置了这个标志位，就代表这个线程是一个deamon线程，后台线程
-        非deamon线程，我们一般叫做工作线程
-        如果工作线程（main线程）结束了，deamon线程不会阻止jvm进程退出
-        deamon线程会跟jvm一起关闭
-         */
-        daemon.setDaemon(true);
-    }
+  public void shutdown() {
+    isRunning = false;
+    daemon.interrupt();
+  }
+
+  /**
+   * 启动后台线程
+   */
+  public void start() {
+    daemon.start();
+  }
+
+  /**
+   * 负责监控微服务存货状态的后台线程
+   */
+  private class Daemon extends Thread {
+    /**
+     * 注册表
+     */
+    private Registry registry = Registry.getInstance();
 
     /**
-     * 启动后台线程
+     * 如果某个服务90秒还没有更新心跳
+     * 就认为他死了
      */
-    public void start() {
-        daemon.start();
-    }
+    @Override
+    public void run() {
+      System.out.println(Thread.currentThread().getName() + "线程的线程组是：" + Thread.currentThread().getThreadGroup());
 
-    /**
-     * 负责监控微服务存货状态的后台线程
-     */
-    private class Daemon extends Thread {
-        /**
-         * 注册表
-         */
-        private Registry registry = Registry.getInstance();
-
-        /**
-         * 如果某个服务90秒还没有更新心跳
-         * 就认为他死了
-         */
-        @Override
-        public void run() {
-            System.out.println(Thread.currentThread().getName() + "线程的线程组是：" + Thread.currentThread().getThreadGroup());
-
-            Map<String, Map<String, ServiceInstance>> registryMap;
-            while (true) {
-                try {
-                    registryMap = registry.getRegistry();
-                    if (registryMap == null || registryMap.size() == 0) {
-                        Thread.sleep(60 * 1000L);
-                        continue;
-                    }
-                    for (String serviceName : registryMap.keySet()) {
-                        Map<String, ServiceInstance> serviceInstanceMap = registryMap.get(serviceName);
-                        if (serviceInstanceMap == null || serviceInstanceMap.size() <= 0) {
-                            System.out.println("注册表内存在服务名称：" + serviceName + "对应map 却为空的数据。");
-                            continue;
-                        }
-                        //监控数据
-                        for (ServiceInstance serviceInstance : serviceInstanceMap.values()) {
-                            //校验服务实例心跳续约时间是否长于过期时间
-                            if (serviceInstance.verifyLoseEfficacy(TTL)) {
-                                //长于过期时间则 认为服务已经宕机 从注册表中删除该服务实例
-                                registry.remove(serviceInstance);
-                            }
-                        }
-                    }
-                    Thread.sleep(60 * 1000L);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+      Map<String, Map<String, ServiceInstance>> registryMap;
+      while (isRunning) {
+        try {
+          registryMap = registry.getRegistry();
+          if (registryMap == null || registryMap.size() == 0) {
+            Thread.sleep(60 * 1000L);
+            continue;
+          }
+          for (String serviceName : registryMap.keySet()) {
+            Map<String, ServiceInstance> serviceInstanceMap = registryMap.get(serviceName);
+            if (serviceInstanceMap == null || serviceInstanceMap.size() <= 0) {
+              System.out.println("注册表内存在服务名称：" + serviceName + "对应map 却为空的数据。");
+              continue;
             }
+            // 监控数据
+            for (ServiceInstance serviceInstance : serviceInstanceMap.values()) {
+              // 校验服务实例心跳续约时间是否长于过期时间
+              if (serviceInstance.verifyLoseEfficacy(TTL)) {
+                // 长于过期时间则 认为服务已经宕机 从注册表中删除该服务实例
+                registry.remove(serviceInstance);
+              }
+            }
+          }
+          Thread.sleep(60 * 1000L);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
         }
+      }
     }
+  }
 }
--- register-server/src/main/java/com/donghao/register/demo/register/server/pojo/ServiceInstance.java
+++ register-server/src/main/java/com/donghao/register/demo/register/server/pojo/ServiceInstance.java
@@ -13,75 +13,80 @@ import lombok.experimental.Accessors;
 @AllArgsConstructor
 @Accessors(chain = true)
 public class ServiceInstance {
-    /**
-     * 服务名称
-     */
-    private String serviceName;
-    /**
-     * ip地址
-     */
-    private String ip;
-    /**
-     * 服务名称
-     */
-    private String hostName;
-    /**
-     * 端口号
-     */
-    private int port;
-    /**
-     * 服务名称
-     */
-    private String serviceInstanceId;
-    /**
-     * 契约
-     */
-    private Lease lease;
+  /**
+   * 服务名称
+   */
+  private String serviceName;
 
-    public ServiceInstance() {
-        this.lease = new Lease();
+  /**
+   * ip地址
+   */
+  private String ip;
+
+  /**
+   * 服务名称
+   */
+  private String hostName;
+
+  /**
+   * 端口号
+   */
+  private int port;
+
+  /**
+   * 服务名称
+   */
+  private String serviceInstanceId;
+
+  /**
+   * 契约
+   */
+  private Lease lease;
+
+  public ServiceInstance() {
+    this.lease = new Lease();
+  }
+
+  /**
+   * 续约心跳时间
+   */
+  public void renew() {
+    lease.renew();
+  }
+
+  /**
+   * 校验服务实例心跳续约时间是否长于过期时间
+   *
+   * @param ttl 过期时间
+   * @return 是否过期
+   */
+  public boolean verifyLoseEfficacy(Long ttl) {
+    if ((System.currentTimeMillis() - lease.getLatestHeartbeatTime()) > ttl) {
+      System.out.println("服务实例不在存活：服务名称-" + this.serviceName + "\t实例-" + this.serviceInstanceId);
+      return false;
     }
+    System.out.println("服务实例存活：服务名称-" + this.serviceName + "\t实例-" + this.serviceInstanceId);
+    return true;
+  }
 
+  /**
+   * 契约
+   * 维护了服务实例和当前的注册中心服务的联系
+   *
+   * @author donghao.wu
+   */
+  @Data
+  @NoArgsConstructor
+  @AllArgsConstructor
+  private class Lease {
     /**
-     * 续约心跳时间
+     * 最近一次心跳的时间
      */
+    private volatile Long latestHeartbeatTime = System.currentTimeMillis();
+
     public void renew() {
-        lease.renew();
+      this.latestHeartbeatTime = System.currentTimeMillis();
+      System.out.println("服务实例：" + serviceInstanceId + "续约成功！续约时间为：" + latestHeartbeatTime);
     }
-
-    /**
-     * 校验服务实例心跳续约时间是否长于过期时间
-     *
-     * @param ttl 过期时间
-     * @return 是否过期
-     */
-    public boolean verifyLoseEfficacy(Long ttl) {
-        if ((System.currentTimeMillis() - lease.getLatestHeartbeatTime()) > ttl) {
-            System.out.println("服务实例不在存活：服务名称-" + this.serviceName + "\t实例-" + this.serviceInstanceId);
-            return false;
-        }
-        System.out.println("服务实例存活：服务名称-" + this.serviceName + "\t实例-" + this.serviceInstanceId);
-        return true;
-    }
-
-    /**
-     * 契约
-     * 维护了服务实例和当前的注册中心服务的联系
-     *
-     * @author donghao.wu
-     */
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    private class Lease {
-        /**
-         * 最近一次心跳的时间
-         */
-        private Long latestHeartbeatTime = System.currentTimeMillis();
-
-        public void renew() {
-            this.latestHeartbeatTime = System.currentTimeMillis();
-            System.out.println("服务实例：" + serviceInstanceId + "续约成功！续约时间为：" + latestHeartbeatTime);
-        }
-    }
+  }
 }

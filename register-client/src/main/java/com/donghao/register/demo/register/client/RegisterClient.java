fa0bdcdabe15054f37a24bed86a4836083def5aa
donghao <346813955@qq.com>
2022-11-29 11:51:32 +0800

    join使用

--- register-client/src/main/java/com/donghao/register/demo/register/client/RegisterClient.java
+++ register-client/src/main/java/com/donghao/register/demo/register/client/RegisterClient.java
@@ -1,14 +1,12 @@
 package com.donghao.register.demo.register.client;
 
+import java.util.UUID;
+
 import com.donghao.register.demo.register.client.pojo.HeartbeatRequest;
 import com.donghao.register.demo.register.client.pojo.RegisterRequest;
 import com.donghao.register.demo.register.client.pojo.RegisterResponse;
 import com.donghao.register.demo.register.client.tools.HttpSender;
 import com.donghao.register.demo.register.common.constants.ResponseCodeConstants;
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
-
-import java.util.UUID;
 
 /**
  * 在服务上被创建和启动,负责和register-server进行通信
@@ -16,107 +14,116 @@ import java.util.UUID;
  * @author donghao.wu
  */
 public class RegisterClient {
-    private static final Logger logger = LoggerFactory.getLogger(RegisterClient.class);
 
-    private String serviceInstanceId;
+  private String serviceInstanceId;
 
-    public RegisterClient() {
-        this.serviceInstanceId = UUID.randomUUID().toString().replace("-", "");
+  public static final String SERVICE_NAME = "inventory-service";
+
+  public static final String IP = "127.0.0.1";
+
+  public static final String HOSTNAME = "inventory01";
+
+  public static final int PORT = 9000;
+
+  /**
+   * 是否完成服务注册
+   */
+  private Boolean finishedRegister;
+
+  /**
+   * http通讯组件
+   */
+  private HttpSender httpSender;
+
+  /**
+   * http通讯组件
+   */
+  private static volatile boolean isRunning;
+
+  public RegisterClient() {
+    httpSender = new HttpSender();
+    finishedRegister = false;
+    isRunning = true;
+    this.serviceInstanceId = UUID.randomUUID().toString().replace("-", "");
+  }
+
+  public void start() {
+    RegisterWorker registerWorker = new RegisterWorker();
+    HeartbeatWorker heartbeatWorker = new HeartbeatWorker();
+    try {
+      /*
+       * 一旦启动组件之后:
+       * 1.想register-server发送请求,注册这个服务
+       * 2.注册成功后,开启另一个进程发送心跳
+       */
+      registerWorker.start();
+      registerWorker.join();
+      heartbeatWorker.start();
+    } catch (InterruptedException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public void start() {
+  /**
+   * 负责向register-server发送注册的线程
+   *
+   * @author donghao.wu
+   */
+  private class RegisterWorker extends Thread {
+    @Override
+    public void run() {
+      if (!finishedRegister) {
         /*
-        一旦启动组件之后:
-        1.想register-server发送请求,注册这个服务
-        2.注册成功后,开启另一个进程发送信条
+         * 1.获取当前机器信息
+         * 2.获取当前机器的IP地址,hostname,以及配置服务监听的服务端口号
+         * 从配置文件中获取
+         * todo 参数放到yml里
          */
-        RegisterWorker registerWorker = new RegisterWorker(serviceInstanceId, logger);
-        registerWorker.start();
-    }
-
-    /**
-     * 负责想register-server发送注册的线程
-     *
-     * @author donghao.wu
-     */
-    private class RegisterWorker extends Thread {
-        public static final String SERVICE_NAME = "inventory-service";
-        public static final String IP = "127.0.0.1";
-        public static final String HOSTNAME = "inventory01";
-        public static final int PORT = 9000;
-        /**
-         * http通讯组件
-         */
-        private HttpSender httpSender;
-        /**
-         * 是否完成服务注册
-         */
-        private Boolean finishedRegister;
-        /**
-         * 服务器实例id
-         */
-        private String serviceInstanceId;
-        private Logger logger;
-
-        /**
-         * Allocates a new {@code Thread} object. This constructor has the same
-         * effect as {@linkplain #(ThreadGroup, Runnable, String) Thread}
-         * {@code (null, null, gname)}, where {@code gname} is a newly generated
-         * name. Automatically generated names are of the form
-         * {@code "Thread-"+}<i>n</i>, where <i>n</i> is an integer.
-         */
-        public RegisterWorker(String serviceInstanceId, Logger logger) {
-            httpSender = new HttpSender();
-            finishedRegister = false;
-            this.serviceInstanceId = serviceInstanceId;
-            this.logger = logger;
+        RegisterRequest req = new RegisterRequest();
+        req.setServiceName(SERVICE_NAME);
+        req.setHostName(HOSTNAME);
+        req.setIp(IP);
+        req.setPort(PORT);
+        req.setServiceInstanceId(serviceInstanceId);
+        System.out.println(this.getClass().getName() + ".实例id:" + serviceInstanceId + ".开始注册");
+        // 注册
+        RegisterResponse register = httpSender.register(req);
+        System.out.println(this.getClass().getName() + "服务注册结果" + register.toString());
+        if (ResponseCodeConstants.SUCCESS.equals(register.getCode())) {
+          finishedRegister = true;
         }
+      }
+    }
+  }
 
-        @Override
-        public void run() {
-            if (!finishedRegister) {
+  /**
+   * 心跳线程
+   *
+   * @author donghao.wu
+   */
+  private class HeartbeatWorker extends Thread {
 
-        /*
-        1.获取当前机器信息
-        2.获取当前机器的IP地址,hostname,以及配置服务监听的服务端口号
-        从配置文件中获取
-        todo 参数放到yml里
-         */
-                RegisterRequest req = new RegisterRequest();
-                req.setServiceName(SERVICE_NAME);
-                req.setHostName(HOSTNAME);
-                req.setIp(IP);
-                req.setPort(PORT);
-                req.setServiceInstanceId(serviceInstanceId);
-                logger.debug(this.getClass().getName() + ".实例id:" + serviceInstanceId + ".开始注册");
-                // 注册
-                RegisterResponse register = httpSender.register(req);
-                logger.debug(this.getClass().getName() + "服务注册结果" + register.toString());
-                if (ResponseCodeConstants.SUCCESS.equals(register.getCode())) {
-                    finishedRegister = true;
-                } else {
-                    return;
-                }
-                if (finishedRegister) {
-                    HeartbeatRequest heartbeat = new HeartbeatRequest();
-                    heartbeat.setServiceInstanceId(serviceInstanceId);
-                    heartbeat.setServiceName(SERVICE_NAME);
-                    while (true) {
-                        // 不停地发送心跳
-                        try {
-                            //每隔30秒执行一次心跳
-                            Thread.sleep(30 * 1000L);
-                            RegisterResponse heatbeatRessponse = httpSender.heatbeat(heartbeat);
-                            //
-                            if (!ResponseCodeConstants.SUCCESS.equals(heatbeatRessponse.getCode())) {
-                                logger.warn(this.getClass().getName() + ":心跳请求报错.内容为:" + heatbeatRessponse.toString());
-                            }
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
+    @Override
+    public void run() {
+      if (finishedRegister) {
+        HeartbeatRequest heartbeat = new HeartbeatRequest();
+        heartbeat.setServiceInstanceId(serviceInstanceId);
+        heartbeat.setServiceName(SERVICE_NAME);
+        while (true) {
+          // 不停地发送心跳
+          try {
+            // 每隔30秒执行一次心跳
+            Thread.sleep(30 * 1000L);
+            RegisterResponse heatbeatRessponse = httpSender.heatbeat(heartbeat);
+            //
+            if (!ResponseCodeConstants.SUCCESS.equals(heatbeatRessponse.getCode())) {
+              System.out.println(this.getClass().getName() + ":心跳请求报错.内容为:" + heatbeatRessponse.toString());
             }
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
         }
+      }
     }
+  }
 }
--- register-client/src/main/java/com/donghao/register/demo/register/client/tools/HttpSender.java
+++ register-client/src/main/java/com/donghao/register/demo/register/client/tools/HttpSender.java
@@ -1,19 +1,16 @@
 package com.donghao.register.demo.register.client.tools;
 
+import static com.donghao.register.demo.register.common.constants.ResponseCodeConstants.FAILURE_TWO;
+
+import java.io.IOException;
+
 import com.alibaba.fastjson.JSONObject;
-import com.donghao.register.demo.register.client.constants.ResponseStatusConstants;
 import com.donghao.register.demo.register.client.pojo.HeartbeatRequest;
 import com.donghao.register.demo.register.client.pojo.RegisterRequest;
 import com.donghao.register.demo.register.client.pojo.RegisterResponse;
 import com.donghao.register.demo.register.common.constants.ResponseStatusConstants;
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+
 import okhttp3.*;
-
-import java.io.IOException;
-
-import static com.donghao.register.demo.register.client.constants.ResponseCodeConstants.FAILURE_TWO;
-import static com.donghao.register.demo.register.common.constants.ResponseCodeConstants.FAILURE_TWO;
 
 /**
  * 负责发送http请求的组件
@@ -21,82 +18,81 @@ import static com.donghao.register.demo.register.common.constants.ResponseCodeCo
  * @author donghao.wu
  */
 public class HttpSender {
-    private static final String URL = "";
-    private static final Logger logger = LoggerFactory.getLogger(HttpSender.class);
+  private static final String URL = "";
 
-    public RegisterResponse register(RegisterRequest req) {
-        /*
-        构造请求,放入示例名称,服务名称,ip地址
-        端口号等
-         */
-        logger.debug(this.getClass().getSimpleName() + "服务实例发送请求进行注册");
-        OkHttpClient client = new OkHttpClient();
-        RequestBody body = RequestBody.create(MediaType.parse("application/json"), JSONObject.toJSONString(req));
-        Request request = new Request.Builder().url(URL).post(body).build();
-        RegisterResponse res = new RegisterResponse();
-        try {
-            Response execute = client.newCall(request).execute();
-            res.setStatus(execute.isSuccessful() ? ResponseStatusConstants.SUCCESS : ResponseStatusConstants.FAILURE);
-            res.setCode(execute.code());
-        } catch (IOException e) {
-            e.printStackTrace();
-            //直接打message
-            logger.warn(e.getMessage());
-            res.setCode(FAILURE_TWO);
-            res.setStatus(ResponseStatusConstants.FAILURE);
-            res.setMessage(e.getMessage());
-            //todo 可以增加重试逻辑
-            /*
-            重试逻辑设计
-            异步
-            内循环 3次 每秒重试一次 3次都失败则写入内存队列
-            定时任务:
-            每分钟执行一次;执行15次后,若内存队列无内容调整调整为半小时执行一次
-             */
-        }
-        System.out.println(res);
-        return res;
-    }
-
-    /**
-     * 发送心跳
-     *
-     * @param req 入参
-     * @return http心跳请求返回值
+  public RegisterResponse register(RegisterRequest req) {
+    /*
+     * 构造请求,放入示例名称,服务名称,ip地址
+     * 端口号等
      */
-    public RegisterResponse heatbeat(HeartbeatRequest req) {
-        /*
-        发送请求进行心跳
-        构造请求,放入示例名称,服务名称,ip地址
-        端口号等
-         */
-        logger.debug(this.getClass().getSimpleName() + "服务实例发送请求进行注册");
-        OkHttpClient client = new OkHttpClient();
-        RequestBody body = RequestBody.create(MediaType.parse("application/json"), JSONObject.toJSONString(req));
-        Request request = new Request.Builder().url(URL).post(body).build();
-        RegisterResponse res = new RegisterResponse();
-        try {
-            logger.debug("发送请求");
-            Response execute = client.newCall(request).execute();
-            res.setStatus(execute.isSuccessful() ? ResponseStatusConstants.SUCCESS : ResponseStatusConstants.FAILURE);
-            res.setCode(execute.code());
-        } catch (IOException e) {
-            e.printStackTrace();
-            //直接打message
-            logger.warn(e.getMessage());
-            res.setCode(FAILURE_TWO);
-            res.setStatus(ResponseStatusConstants.FAILURE);
-            res.setMessage(e.getMessage());
-            //todo 可以增加重试逻辑
-            /*
-            重试逻辑设计
-            异步
-            内循环 3次 每秒重试一次 3次都失败则写入内存队列
-            定时任务:
-            每分钟执行一次;执行15次后,若内存队列无内容调整调整为半小时执行一次
-             */
-        }
-        System.out.println(res);
-        return res;
+    System.out.println(this.getClass().getSimpleName() + "服务实例发送请求进行注册");
+    OkHttpClient client = new OkHttpClient();
+    RequestBody body = RequestBody.create(MediaType.parse("application/json"), JSONObject.toJSONString(req));
+    Request request = new Request.Builder().url(URL).post(body).build();
+    RegisterResponse res = new RegisterResponse();
+    try {
+      Response execute = client.newCall(request).execute();
+      res.setStatus(execute.isSuccessful() ? ResponseStatusConstants.SUCCESS : ResponseStatusConstants.FAILURE);
+      res.setCode(execute.code());
+    } catch (IOException e) {
+      e.printStackTrace();
+      // 直接打message
+      System.out.println(e.getMessage());
+      res.setCode(FAILURE_TWO);
+      res.setStatus(ResponseStatusConstants.FAILURE);
+      res.setMessage(e.getMessage());
+      // todo 可以增加重试逻辑
+      /*
+       * 重试逻辑设计
+       * 异步
+       * 内循环 3次 每秒重试一次 3次都失败则写入内存队列
+       * 定时任务:
+       * 每分钟执行一次;执行15次后,若内存队列无内容调整调整为半小时执行一次
+       */
     }
+    System.out.println(res);
+    return res;
+  }
+
+  /**
+   * 发送心跳
+   *
+   * @param req 入参
+   * @return http心跳请求返回值
+   */
+  public RegisterResponse heatbeat(HeartbeatRequest req) {
+    /*
+     * 发送请求进行心跳
+     * 构造请求,放入示例名称,服务名称,ip地址
+     * 端口号等
+     */
+    System.out.println(this.getClass().getSimpleName() + "服务实例发送请求进行注册");
+    OkHttpClient client = new OkHttpClient();
+    RequestBody body = RequestBody.create(MediaType.parse("application/json"), JSONObject.toJSONString(req));
+    Request request = new Request.Builder().url(URL).post(body).build();
+    RegisterResponse res = new RegisterResponse();
+    try {
+      System.out.println("发送请求");
+      Response execute = client.newCall(request).execute();
+      res.setStatus(execute.isSuccessful() ? ResponseStatusConstants.SUCCESS : ResponseStatusConstants.FAILURE);
+      res.setCode(execute.code());
+    } catch (IOException e) {
+      e.printStackTrace();
+      // 直接打message
+      System.out.println(e.getMessage());
+      res.setCode(FAILURE_TWO);
+      res.setStatus(ResponseStatusConstants.FAILURE);
+      res.setMessage(e.getMessage());
+      // todo 可以增加重试逻辑
+      /*
+       * 重试逻辑设计
+       * 异步
+       * 内循环 3次 每秒重试一次 3次都失败则写入内存队列
+       * 定时任务:
+       * 每分钟执行一次;执行15次后,若内存队列无内容调整调整为半小时执行一次
+       */
+    }
+    System.out.println(res);
+    return res;
+  }
 }
